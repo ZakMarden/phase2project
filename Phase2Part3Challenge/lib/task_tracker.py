bdc7b24f205efc551a1db3f1523fd037892ec426
Zak Marden <zakmarden@hotmail.com>
2023-11-16 12:39:42 +0000

    Phase 2 part 3 challenge DONE

--- Phase2Part3Challenge/lib/task_tracker.py
+++ Phase2Part3Challenge/lib/task_tracker.py
@@ -1,2 +1,10 @@
-def task_tracker():
-    return True
+def task_tracker(text, task):
+    words_list = text.split()
+    task_positions = []
+    if task in words_list:
+        for word_index in range(len(words_list)):
+            if words_list[word_index] == task:
+                task_positions.append(str(word_index + 1))
+        return f"{words_list.count(task)} task(s) found in this text at position(s): " + ", ".join(task_positions)
+    else:
+        return "There are no tasks in this text."
--- Phase2Part3Challenge/tests/test_task_tracker.py
+++ Phase2Part3Challenge/tests/test_task_tracker.py
@@ -1,4 +1,31 @@
 from lib.task_tracker import *
 
-def test_task_tracker():
-    assert task_tracker() == True
+
+"""
+As a user
+So that I can keep track of my tasks
+I want to check if a text includes the string #TODO.
+
+"""
+
+"""
+Function Signature
+Name: task_tracker()
+Arguments: two strings, text and string to check for
+Returns: String telling user how many tasks there are, and their positions in the text
+"""
+
+#Test 1: return number of tasks if above 0
+
+def test_return_true_if_found():
+    assert task_tracker("1 2 3 #TODO 5", "#TODO") == "1 task(s) found in this text at position(s): 4"
+
+#Test 2: return "There are no tasks in this text" if string is not in the text
+
+def test_return_false_if_not_found():
+    assert task_tracker("1 2 3", "#TODO") == "There are no tasks in this text."
+
+#Test 3: check for multiple tasks in text
+
+def test_for_mult_tasks():
+    assert task_tracker("1 #TODO 3 #TODO 5", "#TODO") == "2 task(s) found in this text at position(s): 2, 4"
