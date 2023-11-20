a11ab6671738680aa6797306fc930a8d83e4e2f1
Zak Marden <zakmarden@hotmail.com>
2023-11-20 13:17:43 +0000

    06 Design a Class Challenge DONE

--- 06 Design a Class/lib/music_tracker.py
+++ 06 Design a Class/lib/music_tracker.py
@@ -6,4 +6,23 @@ I want to add tracks I've listened to and see a list of them.
 
 class music_tracker():
     def __init__(self):
-        pass
+        self.track_list = []
+
+    def add_track(self, track):
+        if track not in self.track_list:
+            self.track_list.append(track)
+        else:
+            print("Track not added, already in list!")
+        return
+
+    def list_tracks(self):
+        print("Tracks listened to:\n" + "\n".join(self.track_list))
+        return "Tracks listened to:\n" + "\n".join(self.track_list)
+    
+test = music_tracker()
+test.add_track("track1")
+test.add_track("track2")
+test.add_track("track3")
+test.list_tracks()
+test.add_track("track3")
+test.list_tracks()
--- 06 Design a Class/tests/test_music_tracker.py
+++ 06 Design a Class/tests/test_music_tracker.py
@@ -1,2 +1,32 @@
 from lib.music_tracker import *
 
+#test returns blank list
+
+def test_blank_list():
+    test = music_tracker()
+    assert test.list_tracks() == "Tracks listened to:\n"
+
+#test add track to list
+
+def test_add_track():
+    test = music_tracker()
+    test.add_track("track1")
+    assert test.list_tracks() == "Tracks listened to:\ntrack1"
+
+#test add multiple tracks
+
+def test_add_mult_tracks():
+    test = music_tracker()
+    test.add_track("track1")
+    test.add_track("track2")
+    test.add_track("track3")
+    assert test.list_tracks() == "Tracks listened to:\ntrack1\ntrack2\ntrack3"
+
+#test add track already listened to
+
+def test_add_already_listened():
+    test = music_tracker()
+    test.add_track("track1")
+    test.add_track("track2")
+    test.add_track("track2")
+    assert test.list_tracks() == "Tracks listened to:\ntrack1\ntrack2"
